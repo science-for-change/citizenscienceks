34bf7c15c47e845e66f43144453d3e2a86b6d7a7
Valtteri Karesto <valtteri.karesto@gmail.com>
2014-12-08 10:02:11 +0200

    New layout for Citizen Science

--- public/javascripts/frontpage.js
+++ public/javascripts/frontpage.js
@@ -1,21 +1,6 @@
 $(document).ready(
   function() {
 
-    // resize hero image to 100% height
-    $(".hero").height($(window).height());
-
-    // animation for top bar background
-    setInterval(function() {
-      if ($('.fixed').is(':hidden')) return;
-      var nav = $('.full-width-top-bar');
-      var bg = $('.top-bar-background');
-      if (nav.offset().top > 50 && bg.is(':hidden')) {
-        bg.fadeIn();
-      } else if (nav.offset().top < 50 && bg.is(':visible')) {
-        bg.fadeOut();
-      }
-    }, 500);
-
     // handle window resize for hero image
     $(window).resize(function() {
       $(".hero").height($(window).height());
