c1018e06fde54d2d80664e82fa9364d241e42262
Jani Turunen <turunen.jani@gmail.com>
2014-12-09 10:30:42 +0200

    barebones blog working

--- app/controllers/blog_posts.rb
+++ app/controllers/blog_posts.rb
@@ -1,7 +1,7 @@
 Citizenscienceks::App.controllers :blog_posts do
 
   get :index do
-    @blog_posts = BlogPost.all(:order => 'created_at desc')
+    @blog_posts = BlogPost.all.order('created_at desc')
     render 'blog_posts/index'
   end
 
