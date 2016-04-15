848284b081c793540a7161ffd856dba491380f79
Jani Turunen <turunen.jani@gmail.com>
2016-04-16 00:45:59 +0300

    Change to use S3

--- lib/uploader.rb
+++ lib/uploader.rb
@@ -1,36 +1,20 @@
 class Uploader < CarrierWave::Uploader::Base
 
   ##
-  # Image manipulator library:
-  #
-  # include CarrierWave::RMagick
-  # include CarrierWave::ImageScience
-  # include CarrierWave::MiniMagick
-
-  ##
   # Storage type
   #
-  storage :file
-  # configure do |config|
-  #   config.fog_credentials = {
-  #     :provider              => 'XXX',
-  #     :aws_access_key_id     => 'YOUR_ACCESS_KEY',
-  #     :aws_secret_access_key => 'YOUR_SECRET_KEY'
-  #   }
-  #   config.fog_directory = 'YOUR_BUCKET'
-  # end
-  # storage :fog
-
-
-
-  ## Manually set root
-  def root; File.join(Padrino.root,"public/"); end
-
-  ##
-  # Directory where uploaded files will be stored (default is /public/uploads)
-  #
-  def store_dir
-    'images/uploads'
+  #storage :file
+  storage :fog
+  configure do |config|
+    config.fog_credentials = {
+      provider:               'AWS',
+      aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
+      region:                 'eu-central-1'
+    }
+    config.fog_public     = true
+    config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
+    config.fog_directory  = 'citizenscienceks'
   end
 
   ##
@@ -40,38 +24,13 @@ class Uploader < CarrierWave::Uploader::Base
     Padrino.root("tmp")
   end
 
-  ##
-  # Default URL as a default if there hasn't been a file uploaded
-  #
-  # def default_url
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  ##
-  # Process files as they are uploaded.
-  #
-  # process :resize_to_fit => [740, 580]
-
-  ##
-  # Create different versions of your uploaded files
-  #
-  # version :header do
-  #   process :resize_to_fill => [940, 250]
-  #   version :thumb do
-  #     process :resize_to_fill => [230, 85]
-  #   end
-  # end
-  ##
   # White list of extensions which are allowed to be uploaded:
   #
   def extension_white_list
-    %w(jpg jpeg gif png)
+    %w(txt kmz)
   end
 
-  ##
-  # Override the filename of the uploaded files
-  #
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    "#{original_filename}"
+  end
 end
